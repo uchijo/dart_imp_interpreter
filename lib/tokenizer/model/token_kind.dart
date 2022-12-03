b9791fb5bbea9eea1714cd26895b5fed47f9c2e9
Uchijo <kgtnttki.yuta@gmail.com>
2022-12-04 01:03:07 +0900

    予約語をいくつか追加

--- lib/tokenizer/model/token_kind.dart
+++ lib/tokenizer/model/token_kind.dart
@@ -1,8 +1,12 @@
 enum TokenKind {
-  iop,
-  bop,
-  integer,
-  boolean,
-  variable,
-  control,
+  iop(['+', '-']),
+  bop(['=', '<', '>']),
+  integer(null),
+  boolean(['true', 'false']),
+  variable(null),
+  control([':=', ';', 'skip', 'if', 'then', 'else', 'while', 'do']);
+
+  const TokenKind(this.words);
+
+  final List<String>? words;
 }
---
+++ lib/tokenizer/tokenizer.dart
@@ -0,0 +1,6 @@
+import 'package:dart_imp_interpreter/tokenizer/model/token.dart';
+
+List<Token> tokenized({required String rawInput}) {
+  
+  return [];
+}
