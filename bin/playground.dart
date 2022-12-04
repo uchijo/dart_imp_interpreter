846c25252d87d82d7152265a6f42cfbbb28b8bfc
Uchijo <kgtnttki.yuta@gmail.com>
2022-12-04 23:33:36 +0900

    予約語を取得できるようにした

---
+++ bin/playground.dart
@@ -0,0 +1,6 @@
+import 'package:dart_imp_interpreter/tokenizer/reserved_words.dart';
+
+void main() {
+  print('reserved symbols: $reservedSymbols');
+  print('reserved words: $reservedWords');
+}
---
+++ lib/tokenizer/reserved_words.dart
@@ -0,0 +1,32 @@
+import 'package:dart_imp_interpreter/tokenizer/model/token_kind.dart';
+
+final reservedWords = TokenKind.values.fold(
+  <String>[],
+  (previousValue, element) {
+    previousValue.addAll(element.words ?? []);
+    return previousValue;
+  },
+);
+
+final _nonReservedSymbolPattern = RegExp(r'[a-zA-Z0-9]+');
+
+final reservedSymbols = TokenKind.values.fold(
+  <String>[],
+  (previousValue, element) {
+    if (element.words == null) {
+      return previousValue;
+    }
+
+    for (final word in element.words!) {
+      // 英数字のみ含むものは予約語に含まれる記号じゃない
+      if (_nonReservedSymbolPattern.hasMatch(word)) {
+        print('matched: $word, skip this loop');
+        continue;
+      }
+      print('not continue');
+      previousValue.add(word);
+    }
+
+    return previousValue;
+  },
+);
